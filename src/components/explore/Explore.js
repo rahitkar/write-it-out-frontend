e05f5f6c9ffa08ec7623dc5c5de078de86dca12f
rahit_kar <rahit@noreplay.com>
2020-09-18 13:43:40 +0530

    added explore component

--- src/components/explore/Explore.js
+++ src/components/explore/Explore.js
@@ -6,11 +6,29 @@ import { BrowserRouter } from 'react-router-dom';
 const Home = (props) => <h1>Home</h1>;
 const Editor = (props) => <h1>Editor</h1>;
 
+const names = [
+  'All',
+  'Inspiration',
+  'Letter',
+  'Life',
+  'Love',
+  'One-liner',
+  'Philosophy',
+  'Poetry',
+  'Politics',
+  'Short-story',
+];
+
 export default (props) => {
   return (
     <BrowserRouter>
-      <Links names={['home', 'write']} />
-      <Gallery names={['home', 'write']} components={[<Home />, <Editor />]} />
+      <Links
+        names={names}
+        activeClass='indicate-1'
+        class='option-1'
+        rapperClass='top-bar-1'
+      />
+      <Gallery names={names} components={[<Home />, <Editor />]} />
     </BrowserRouter>
   );
 };
