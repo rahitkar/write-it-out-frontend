70e7f4bc0b32984ba1911f4c5ee95d2a25f2489b
rahit_kar <rahit@noreplay.com>
2020-09-23 02:45:36 +0530

    styled the login page

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import api from './api';
+import React from 'react';
+import './container.css';
+import Pen from './pen.png';
 
 export default () => {
-
   return (
-    <div>
+    <div className='login'>
+      <div>
+        <div>WriteItOut</div>
+        <img src={Pen} alt='pen' />
+      </div>
       <button>
-        <a href='https://github.com/login/oauth/authorize?client_id=de3c6a3996f107813577'>
-          Login
+        <a className='github-login' href='https://github.com/login/oauth/authorize?client_id=de3c6a3996f107813577'>
+          Github Login
         </a>
       </button>
     </div>
