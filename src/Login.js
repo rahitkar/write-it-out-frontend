81f7f9fded9fdcdd0fa204e60e4f04c27c4f86db
rahit_kar <rahit@noreplay.com>
2020-09-24 18:21:45 +0530

    styled login

--- src/Login.js
+++ src/Login.js
@@ -9,14 +9,12 @@ export default () => {
         <div>WriteItOut</div>
         <img src={Pen} alt='pen' />
       </div>
-      <button>
-        <a
-          className='github-login'
-          href='https://github.com/login/oauth/authorize?client_id=de3c6a3996f107813577'
-        >
-          Github Login
-        </a>
-      </button>
+      <a
+        className='github-login'
+        href='https://github.com/login/oauth/authorize?client_id=de3c6a3996f107813577'
+      >
+        Github Login
+      </a>
     </div>
   );
 };
